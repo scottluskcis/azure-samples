eb72464ae31844a09b3458a6e526b678e363139c
Scott Lusk <slusk@hitachisolutions.com>
2020-11-06 09:10:11 -0500

    dotnet new console to add blob storage project

---
+++ dotnet/storage/blob/blob-storage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blob_storage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
